5faa06333f13ee2bc0b0f821202c461ee209e303
ishkhan <ishkhan.n23@gmail.com>
2018-12-22 08:02:17 +0000

    Color Api changed , Now saving as JSON

--- app/src/main/java/com/infinity/ishkhan/red/Heart.kt
+++ app/src/main/java/com/infinity/ishkhan/red/Heart.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_heart.*
 import kotlinx.android.synthetic.main.fragment_heart.view.*
+import org.json.JSONObject
 import java.io.File
 import java.util.*
 
@@ -23,11 +24,11 @@ class Heart : Fragment() {
         val view = inflater.inflate(R.layout.fragment_heart, container, false)
 
         try {
-            val theColors = File(context!!.filesDir, "theColors").readLines()
-            val id = Random().nextInt(theColors.size)
-            val color = theColors[id].split(":")
-            view.mainCard.setCardBackgroundColor(color[1].toInt())
-            view.colorName.text = color[0]
+            val theColors = JSONObject(File(context!!.filesDir, "theColors").readText()).getJSONArray("colors")
+            val id = Random().nextInt(theColors.length())
+            val color = theColors.getJSONObject(id)
+            view.mainCard.setCardBackgroundColor(color.getInt("color"))
+            view.colorName.text = color.getString("name")
         }
         catch (e:Exception){
             view.mainCard.setCardBackgroundColor(Color.BLACK)
